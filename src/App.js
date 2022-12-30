6cbf82e1853b95a84f1f74b76026b9dee71bdee2
Anu S <anusaratha97@gmail.com>
2022-12-30 21:53:32 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
+import SignUp from './Pages/SignUp';
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route path='/signup'>
+          <SignUp />
+        </Route>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/SignUp/SignUp.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './SignUp.css';
+
+export default function SignUp() {
+  return (
+    <div className="signupParentDiv">
+        <form>
+          <label htmlFor="fname">Username</label>
+          <br />
+          <input
+            className="input"
+            type="text"
+            id="fname"
+            name="name"
+            defaultValue="John"
+          />
+          <br />
+          <label htmlFor="fname">Email</label>
+          <br />
+          <input
+            className="input"
+            type="email"
+            id="fname"
+            name="email"
+            defaultValue="John"
+          />
+          <br />
+          <label htmlFor="lname">Phone</label>
+          <br />
+          <input
+            className="input"
+            type="number"
+            id="lname"
+            name="phone"
+            defaultValue="Doe"
+          />
+          <br />
+          <label htmlFor="lname">Password</label>
+          <br />
+          <input
+            className="input"
+            type="password"
+            id="lname"
+            name="password"
+            defaultValue="Doe"
+          />
+          <br />
+          <br />
+          <button>SignUp</button>
+        </form>
+        <a href='/'>Login</a>
+      </div>
+  );
+}
