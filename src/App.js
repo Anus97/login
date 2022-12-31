0478f9d8497eb3d0a6e23846772fe21beeba28f0
Anu S <anusaratha97@gmail.com>
2022-12-31 21:01:12 +0530

    changes added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
-import SignUp from './Pages/SignUp';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Route exact path='/'>
-          <Home/>
-        </Route>
-        <Route path='/signup'>
-          <SignUp />
-        </Route>
-      </Router>
+      <Home/>
     </div>
   );
 }
