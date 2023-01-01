5e80fc7d2c08c5c491ebc6a1cbb7dee73d6b64ed
Anu S <anusaratha97@gmail.com>
2023-01-01 19:30:31 +0530

    commited

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { FirebaseContext } from '../../stores/FirebaseContext';
 import './SignUp.css';
 
 export default function SignUp() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setUPassword] = useState('');
+  const {firebase} = useContext(FirebaseContext)
+
+  const submit = (e)=>{
+    e.preventDefault()
+    console.log(firebase);
+  }
   return (
     <div className="signupParentDiv">
-        <form>
+        <form onSubmit={submit}>
           <label htmlFor="fname">Username</label>
           <br />
           <input
             className="input"
             type="text"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
             id="fname"
             name="name"
             defaultValue="John"
@@ -20,6 +33,8 @@ export default function SignUp() {
           <input
             className="input"
             type="email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
             id="fname"
             name="email"
             defaultValue="John"
@@ -30,6 +45,8 @@ export default function SignUp() {
           <input
             className="input"
             type="number"
+            value={phone}
+            onChange={(e)=>setPhone(e.target.value)}
             id="lname"
             name="phone"
             defaultValue="Doe"
@@ -40,6 +57,8 @@ export default function SignUp() {
           <input
             className="input"
             type="password"
+            value={password}
+            onChange={(e)=>setUPassword(e.target.value)}
             id="lname"
             name="password"
             defaultValue="Doe"
